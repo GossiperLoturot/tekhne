6302200ad38cf8f1f24d0c083a1d8ef59aa9394c
GossiperLoturot <ts20030211@gmail.com>
2023-03-25 14:24:05 +0900

    fix BoundsIntExt

--- Assets/Scripts/BoundsIntExt.cs
+++ Assets/Scripts/BoundsIntExt.cs
@@ -10,4 +10,13 @@ public static class BoundsIntExt
 
         return !isOutOfBounds;
     }
+
+    public static bool Contains(this BoundsInt bounds, Vector3 point)
+    {
+        var isOutOfBounds = point.x < bounds.xMin || bounds.xMax < point.x
+            || point.y < bounds.yMin || bounds.yMax < point.y
+            || point.z < bounds.zMin || bounds.zMax < point.z;
+
+        return !isOutOfBounds;
+    }
 }
