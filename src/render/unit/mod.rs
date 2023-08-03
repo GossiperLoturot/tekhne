cdf3b63e700140a5c4b2b1ad4b880c7381af88d4
GossiperLoturot <ts20030211@gmail.com>
2023-08-04 00:03:47 +0900

    Revert ":recycle: refactor: unit rendering module"
    This reverts commit dc6599daf62f106a4c3f97a0858ad4bb53c81042.

--- src/render/unit/mod.rs
+++ src/render/unit/mod.rs
@@ -1,22 +1,74 @@
-use self::{
-    extract::{UnitAtlasItem, UnitShapeItem},
-    instance::{UnitInstance, UnitInstanceResource},
-    shape::{UnitShapeResource, UnitVertex},
-    texture::UnitAtlasResource,
-};
+use self::{texture::UnitTextureResource, unit_kind::UnitShape};
 use super::{CameraResource, DepthResource};
 use crate::service::Service;
+use ahash::AHashMap;
+use glam::*;
 use strum::IntoEnumIterator;
+use wgpu::util::DeviceExt;
 
-mod extract;
-mod instance;
-mod shape;
 mod texture;
+mod unit_kind;
+
+#[repr(C)]
+#[derive(Clone, Copy, bytemuck::Pod, bytemuck::Zeroable)]
+struct Vertex {
+    position: [f32; 3],
+    texcoord: [f32; 2],
+}
+
+impl Vertex {
+    const ATTRIBUTES: &[wgpu::VertexAttribute] =
+        &wgpu::vertex_attr_array![0 => Float32x3, 1 => Float32x2];
+
+    fn layout<'a>() -> wgpu::VertexBufferLayout<'a> {
+        wgpu::VertexBufferLayout {
+            array_stride: std::mem::size_of::<Self>() as u64,
+            step_mode: wgpu::VertexStepMode::Vertex,
+            attributes: Self::ATTRIBUTES,
+        }
+    }
+}
+
+#[repr(C)]
+#[derive(Clone, Copy, bytemuck::Pod, bytemuck::Zeroable)]
+struct Instance {
+    position_min: [f32; 3],
+    position_max: [f32; 3],
+    texcoord: [f32; 4],
+}
+
+impl Instance {
+    const ATTRIBUTES: &[wgpu::VertexAttribute] =
+        &wgpu::vertex_attr_array![2 => Float32x3, 3 => Float32x3, 4 => Float32x4];
+
+    fn layout<'a>() -> wgpu::VertexBufferLayout<'a> {
+        wgpu::VertexBufferLayout {
+            array_stride: std::mem::size_of::<Self>() as u64,
+            step_mode: wgpu::VertexStepMode::Instance,
+            attributes: Self::ATTRIBUTES,
+        }
+    }
+}
+
+type ShapeGroupId = UnitShape;
+struct ShapeGroup {
+    id: ShapeGroupId,
+    vertex_buffer: wgpu::Buffer,
+    index_buffer: wgpu::Buffer,
+    index_count: u32,
+}
+
+type InstanceGroupId = (UnitShape, u32);
+struct InstanceGroup {
+    id: InstanceGroupId,
+    instance_buffer: wgpu::Buffer,
+    instance_count: u32,
+}
 
 pub struct UnitPipeline {
-    atlas_resource: UnitAtlasResource,
-    shape_resource: UnitShapeResource,
-    instance_resource: UnitInstanceResource,
+    texture_resource: UnitTextureResource,
+    shape_groups: AHashMap<ShapeGroupId, ShapeGroup>,
+    instance_groups: AHashMap<InstanceGroupId, InstanceGroup>,
     pipeline: wgpu::RenderPipeline,
 }
 
@@ -27,17 +79,93 @@ impl UnitPipeline {
         config: &wgpu::SurfaceConfiguration,
         camera_resource: &CameraResource,
     ) -> Self {
-        let atlas_resource =
-            UnitAtlasResource::new(device, queue, &UnitAtlasItem::iter().collect::<Vec<_>>());
-        let shape_resource =
-            UnitShapeResource::new(device, &UnitShapeItem::iter().collect::<Vec<_>>());
-        let instance_resource = UnitInstanceResource::new(device, &atlas_resource, &shape_resource);
+        let texture_resource = UnitTextureResource::new(device, queue);
+
+        let mut shape_groups = AHashMap::new();
+        for id in UnitShape::iter() {
+            #[rustfmt::skip]
+            let vertices: &[Vertex] = match id {
+                UnitShape::Block => &[
+                    Vertex { position: [0.0, 0.0, 0.0], texcoord: [0.0, 1.0] },
+                    Vertex { position: [1.0, 0.0, 0.0], texcoord: [1.0, 1.0] },
+                    Vertex { position: [1.0, 1.0, 1.0], texcoord: [1.0, 0.0] },
+                    Vertex { position: [0.0, 1.0, 1.0], texcoord: [0.0, 0.0] },
+                ],
+                UnitShape::Top => &[
+                    Vertex { position: [0.0, 0.0, 1.0], texcoord: [0.0, 1.0] },
+                    Vertex { position: [1.0, 0.0, 1.0], texcoord: [1.0, 1.0] },
+                    Vertex { position: [1.0, 1.0, 1.0], texcoord: [1.0, 0.0] },
+                    Vertex { position: [0.0, 1.0, 1.0], texcoord: [0.0, 0.0] },
+                ],
+                UnitShape::Bottom => &[
+                    Vertex { position: [0.0, 0.0, 0.0], texcoord: [0.0, 1.0] },
+                    Vertex { position: [1.0, 0.0, 0.0], texcoord: [1.0, 1.0] },
+                    Vertex { position: [1.0, 1.0, 0.0], texcoord: [1.0, 0.0] },
+                    Vertex { position: [0.0, 1.0, 0.0], texcoord: [0.0, 0.0] },
+                ],
+                UnitShape::Quad => &[
+                    Vertex { position: [0.0, 0.0, 0.0], texcoord: [0.0, 1.0] },
+                    Vertex { position: [1.0, 0.0, 0.0], texcoord: [1.0, 1.0] },
+                    Vertex { position: [1.0, 0.0, 1.0], texcoord: [1.0, 0.0] },
+                    Vertex { position: [0.0, 0.0, 1.0], texcoord: [0.0, 0.0] },
+                ],
+            };
+
+            let indices: &[u16] = &[0, 1, 2, 2, 3, 0];
+            let index_count = indices.len() as u32;
+
+            let vertex_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+                label: None,
+                contents: bytemuck::cast_slice(vertices),
+                usage: wgpu::BufferUsages::VERTEX,
+            });
+
+            let index_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+                label: None,
+                contents: bytemuck::cast_slice(indices),
+                usage: wgpu::BufferUsages::INDEX,
+            });
+
+            shape_groups.insert(
+                id,
+                ShapeGroup {
+                    id,
+                    vertex_buffer,
+                    index_buffer,
+                    index_count,
+                },
+            );
+        }
+
+        let page_count = texture_resource.page_count();
+        let mut instance_groups = AHashMap::new();
+        for shape in UnitShape::iter() {
+            for page in 0..page_count {
+                let id = (shape, page);
+
+                let instance_buffer = device.create_buffer(&wgpu::BufferDescriptor {
+                    label: None,
+                    size: device.limits().max_buffer_size,
+                    usage: wgpu::BufferUsages::VERTEX | wgpu::BufferUsages::COPY_DST,
+                    mapped_at_creation: false,
+                });
+
+                instance_groups.insert(
+                    id,
+                    InstanceGroup {
+                        id,
+                        instance_buffer,
+                        instance_count: 0,
+                    },
+                );
+            }
+        }
 
         let pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
             label: None,
             bind_group_layouts: &[
                 camera_resource.bind_group_layout(),
-                atlas_resource.bind_group_layout(),
+                texture_resource.bind_group_layout(),
             ],
             push_constant_ranges: &[],
         });
@@ -51,7 +179,7 @@ impl UnitPipeline {
             vertex: wgpu::VertexState {
                 module: &shader,
                 entry_point: "vs_main",
-                buffers: &[UnitVertex::layout(), UnitInstance::layout()],
+                buffers: &[Vertex::layout(), Instance::layout()],
             },
             primitive: wgpu::PrimitiveState {
                 topology: wgpu::PrimitiveTopology::TriangleList,
@@ -87,16 +215,71 @@ impl UnitPipeline {
         });
 
         Self {
-            atlas_resource,
-            shape_resource,
-            instance_resource,
+            shape_groups,
+            instance_groups,
+            texture_resource,
             pipeline,
         }
     }
 
     pub fn pre_draw(&mut self, queue: &wgpu::Queue, service: &Service) {
-        self.instance_resource
-            .pre_draw(queue, service, &self.atlas_resource);
+        if let Some(camera) = service.camera.get_camera() {
+            let view_aabb = camera.view_aabb();
+
+            let units = service
+                .unit
+                .get_units(view_aabb)
+                .into_iter()
+                .map(|unit| (unit.position, unit.kind));
+
+            let iunits = service
+                .iunit
+                .get_iunits(view_aabb.as_iaabb3())
+                .into_iter()
+                .map(|iunit| (iunit.position.as_vec3a(), iunit.kind));
+
+            let mut instances = AHashMap::new();
+            for (origin, unit_kind) in Iterator::chain(units, iunits) {
+                let shape = unit_kind.shape();
+                let shape_aabb = unit_kind.shape_size();
+                let atlas_texcoord = self
+                    .texture_resource
+                    .texcoord(&unit_kind)
+                    .unwrap_or_else(|| panic!("not registered unit kind {:?}", &unit_kind));
+
+                let position_min = (origin + shape_aabb.min).into();
+                let position_max = (origin + shape_aabb.max).into();
+                let texcoord = [
+                    atlas_texcoord.x,
+                    atlas_texcoord.y,
+                    atlas_texcoord.width,
+                    atlas_texcoord.height,
+                ];
+
+                instances
+                    .entry((shape, atlas_texcoord.page))
+                    .or_insert(vec![])
+                    .push(Instance {
+                        position_min,
+                        position_max,
+                        texcoord,
+                    });
+            }
+
+            for (id, instances) in instances {
+                let instance_group = self
+                    .instance_groups
+                    .get_mut(&id)
+                    .expect("failed to get instance group");
+
+                instance_group.instance_count = instances.len() as u32;
+                queue.write_buffer(
+                    &instance_group.instance_buffer,
+                    0,
+                    bytemuck::cast_slice(&instances),
+                );
+            }
+        }
     }
 
     pub fn draw<'a>(
@@ -104,38 +287,36 @@ impl UnitPipeline {
         render_pass: &mut wgpu::RenderPass<'a>,
         camera_resouce: &'a CameraResource,
     ) {
-        for page in 0..self.atlas_resource.page_count() {
-            for shape in self.shape_resource.shapes() {
-                let texture_resource = self
-                    .atlas_resource
-                    .bind_group(page)
-                    .expect("failed to get texture resource");
+        for (shape, page) in self.instance_groups.keys().copied() {
+            let shape_group = self
+                .shape_groups
+                .get(&shape)
+                .expect("failed to get shape group");
 
-                let shape_group = self
-                    .shape_resource
-                    .shape_group(shape)
-                    .expect("failed to get shape group");
+            let instance_group = self
+                .instance_groups
+                .get(&(shape, page))
+                .expect("failed to get instance group");
 
-                let instance_group = self
-                    .instance_resource
-                    .instance_group(page, shape)
-                    .expect("failed to get instance group");
+            let texture_resource = self
+                .texture_resource
+                .bind_group(page)
+                .expect("failed to get texture resource");
 
-                render_pass.set_pipeline(&self.pipeline);
-                render_pass.set_bind_group(0, camera_resouce.bind_group(), &[]);
-                render_pass.set_bind_group(1, texture_resource, &[]);
-                render_pass.set_vertex_buffer(0, shape_group.vertex_buffer.slice(..));
-                render_pass.set_vertex_buffer(1, instance_group.instance_buffer.slice(..));
-                render_pass.set_index_buffer(
-                    shape_group.index_buffer.slice(..),
-                    wgpu::IndexFormat::Uint16,
-                );
-                render_pass.draw_indexed(
-                    0..shape_group.index_count,
-                    0,
-                    0..instance_group.instance_count,
-                );
-            }
+            render_pass.set_pipeline(&self.pipeline);
+            render_pass.set_bind_group(0, camera_resouce.bind_group(), &[]);
+            render_pass.set_bind_group(1, texture_resource, &[]);
+            render_pass.set_vertex_buffer(0, shape_group.vertex_buffer.slice(..));
+            render_pass.set_vertex_buffer(1, instance_group.instance_buffer.slice(..));
+            render_pass.set_index_buffer(
+                shape_group.index_buffer.slice(..),
+                wgpu::IndexFormat::Uint16,
+            );
+            render_pass.draw_indexed(
+                0..shape_group.index_count,
+                0,
+                0..instance_group.instance_count,
+            );
         }
     }
 }
--- src/render/unit/texture.rs
+++ src/render/unit/texture.rs
@@ -1,14 +1,12 @@
-use super::extract::UnitAtlasItem;
+use super::unit_kind::TextureOption;
+use crate::model::*;
 use ahash::AHashMap;
+use glam::*;
 use std::collections::BTreeMap;
+use strum::IntoEnumIterator;
 use wgpu::util::DeviceExt;
 
-pub enum UnitAtlasOption {
-    Single,
-    Continuous,
-}
-
-pub struct UnitAtlasTexcoord {
+pub struct AtlasTexcoord {
     pub page: u32,
     pub x: f32,
     pub y: f32,
@@ -16,128 +14,151 @@ pub struct UnitAtlasTexcoord {
     pub height: f32,
 }
 
-pub struct UnitAtlasResource {
+pub struct AtlasResult {
+    pub block_size: u32,
+    pub size_per_block: u32,
+    pub mip_level_count: u32,
+    pub page_count: u32,
+    pub texcoords: AHashMap<UnitKind, AtlasTexcoord>,
+    pub atlas_set: Vec<Vec<image::DynamicImage>>,
+}
+
+pub fn atlas_packing(
+    block_size: u32,
+    size_per_block: u32,
+    max_page_count: u32,
+    items: &[UnitKind],
+) -> Option<AtlasResult> {
+    let mut rects = rectangle_pack::GroupedRectsToPlace::<_, ()>::new();
+    for &item in items {
+        let (width, height) = item.texture_size();
+        let rect = rectangle_pack::RectToInsert::new(width + 2, height + 2, 1);
+        rects.push_rect(item, None, rect);
+    }
+
+    let mut target_bin = BTreeMap::new();
+    target_bin.insert(
+        (),
+        rectangle_pack::TargetBin::new(block_size, block_size, max_page_count),
+    );
+
+    let locations = rectangle_pack::pack_rects(
+        &rects,
+        &mut target_bin,
+        &rectangle_pack::volume_heuristic,
+        &rectangle_pack::contains_smallest_box,
+    )
+    .ok()?;
+
+    let mut texcoords = AHashMap::new();
+
+    let page_count = locations
+        .packed_locations()
+        .iter()
+        .map(|(_, (_, location))| location.z())
+        .max()?
+        + 1;
+    let mip_level_count = size_per_block.ilog2();
+    let mut atlas_set = vec![];
+    for _ in 0..page_count {
+        let mut atlas = vec![];
+        for mip_level in 0..mip_level_count {
+            let size = (size_per_block >> mip_level) * block_size;
+            atlas.push(image::DynamicImage::new_rgba8(size, size));
+        }
+        atlas_set.push(atlas);
+    }
+
+    for (&item, (_, location)) in locations.packed_locations() {
+        println!("{:?} {:?}", item, location);
+
+        let texcoord = AtlasTexcoord {
+            page: location.z(),
+            x: (location.x() + 1) as f32 / block_size as f32,
+            y: (location.y() + 1) as f32 / block_size as f32,
+            width: (location.width() - 2) as f32 / block_size as f32,
+            height: (location.height() - 2) as f32 / block_size as f32,
+        };
+        texcoords.insert(item, texcoord);
+
+        let texture = item.texture()?;
+        let (width, height) = item.texture_size();
+        let texture_option = item.texture_option();
+
+        let mut dilation = image::DynamicImage::new_rgba8(
+            size_per_block * (width + 2),
+            size_per_block * (height + 2),
+        );
+        match texture_option {
+            TextureOption::Single => image::imageops::replace(
+                &mut dilation,
+                &texture,
+                size_per_block as i64,
+                size_per_block as i64,
+            ),
+            TextureOption::Continuous => {
+                for x in -1..=1 {
+                    for y in -1..=1 {
+                        image::imageops::replace(
+                            &mut dilation,
+                            &texture,
+                            size_per_block as i64 + (size_per_block * width) as i64 * x,
+                            size_per_block as i64 + (size_per_block * height) as i64 * y,
+                        );
+                    }
+                }
+            }
+        }
+
+        let atlas = atlas_set.get_mut(location.z() as usize)?;
+        for mip_level in 0..mip_level_count {
+            let size_per_block = size_per_block >> mip_level;
+            let mip_map = dilation.resize(
+                size_per_block * (width + 2),
+                size_per_block * (height + 2),
+                image::imageops::FilterType::Triangle,
+            );
+
+            let target = atlas.get_mut(mip_level as usize)?;
+            image::imageops::replace(
+                target,
+                &mip_map,
+                (size_per_block * location.x()) as i64,
+                (size_per_block * location.y()) as i64,
+            );
+        }
+    }
+
+    Some(AtlasResult {
+        block_size,
+        size_per_block,
+        mip_level_count,
+        page_count,
+        texcoords,
+        atlas_set,
+    })
+}
+
+pub struct UnitTextureResource {
     page_count: u32,
-    texcoords: AHashMap<UnitAtlasItem, UnitAtlasTexcoord>,
+    texcoords: AHashMap<UnitKind, AtlasTexcoord>,
     bind_group_layout: wgpu::BindGroupLayout,
     bind_groups: Vec<wgpu::BindGroup>,
 }
 
-impl UnitAtlasResource {
+impl UnitTextureResource {
     const BLOCK_SIZE: u32 = 32;
     const SIZE_PER_BLOCK: u32 = 32;
     const MAX_PAGE_COUNT: u32 = 255;
 
-    pub fn new(device: &wgpu::Device, queue: &wgpu::Queue, items: &[UnitAtlasItem]) -> Self {
-        let mut rects = rectangle_pack::GroupedRectsToPlace::<_, ()>::new();
-        for &item in items {
-            let (width, height) = item.block_size();
-            let rect = rectangle_pack::RectToInsert::new(width + 2, height + 2, 1);
-            rects.push_rect(item, None, rect);
-        }
-
-        let mut target_bin = BTreeMap::new();
-        target_bin.insert(
-            (),
-            rectangle_pack::TargetBin::new(
-                Self::BLOCK_SIZE,
-                Self::BLOCK_SIZE,
-                Self::MAX_PAGE_COUNT,
-            ),
-        );
-
-        let locations = rectangle_pack::pack_rects(
-            &rects,
-            &mut target_bin,
-            &rectangle_pack::volume_heuristic,
-            &rectangle_pack::contains_smallest_box,
+    pub fn new(device: &wgpu::Device, queue: &wgpu::Queue) -> Self {
+        let atlas_result = atlas_packing(
+            Self::BLOCK_SIZE,
+            Self::SIZE_PER_BLOCK,
+            Self::MAX_PAGE_COUNT,
+            &UnitKind::iter().collect::<Vec<_>>(),
         )
-        .expect("failed to pack atlas");
-
-        let mut texcoords = AHashMap::new();
-
-        let page_count = locations
-            .packed_locations()
-            .iter()
-            .map(|(_, (_, location))| location.z())
-            .max()
-            .expect("failed to compute page count")
-            + 1;
-        let size = Self::SIZE_PER_BLOCK * Self::BLOCK_SIZE;
-        let mip_level_count = Self::SIZE_PER_BLOCK.ilog2();
-        let mut atlas_set = vec![];
-        for _ in 0..page_count {
-            let mut atlas = vec![];
-            for mip_level in 0..mip_level_count {
-                let size = size >> mip_level;
-                atlas.push(image::DynamicImage::new_rgba8(size, size));
-            }
-            atlas_set.push(atlas);
-        }
-
-        for (&item, (_, location)) in locations.packed_locations() {
-            let texcoord = UnitAtlasTexcoord {
-                page: location.z(),
-                x: (location.x() + 1) as f32 / Self::BLOCK_SIZE as f32,
-                y: (location.y() + 1) as f32 / Self::BLOCK_SIZE as f32,
-                width: (location.width() - 2) as f32 / Self::BLOCK_SIZE as f32,
-                height: (location.height() - 2) as f32 / Self::BLOCK_SIZE as f32,
-            };
-            texcoords.insert(item, texcoord);
-
-            let texture = item.texture().expect("failed to load image");
-            let (width, height) = item.block_size();
-            let texture_option = item.atlas_option();
-
-            let mut dilation = image::DynamicImage::new_rgba8(
-                Self::SIZE_PER_BLOCK * (width + 2),
-                Self::SIZE_PER_BLOCK * (height + 2),
-            );
-            match texture_option {
-                UnitAtlasOption::Single => image::imageops::replace(
-                    &mut dilation,
-                    &texture,
-                    Self::SIZE_PER_BLOCK as i64,
-                    Self::SIZE_PER_BLOCK as i64,
-                ),
-                UnitAtlasOption::Continuous => {
-                    for x in -1..=1 {
-                        for y in -1..=1 {
-                            image::imageops::replace(
-                                &mut dilation,
-                                &texture,
-                                Self::SIZE_PER_BLOCK as i64
-                                    + (Self::SIZE_PER_BLOCK * width) as i64 * x,
-                                Self::SIZE_PER_BLOCK as i64
-                                    + (Self::SIZE_PER_BLOCK * height) as i64 * y,
-                            );
-                        }
-                    }
-                }
-            }
-
-            let atlas = atlas_set
-                .get_mut(location.z() as usize)
-                .expect("failed to get atlas by page");
-            for mip_level in 0..mip_level_count {
-                let size_per_block = Self::SIZE_PER_BLOCK >> mip_level;
-                let mip_map = dilation.resize(
-                    size_per_block * (width + 2),
-                    size_per_block * (height + 2),
-                    image::imageops::FilterType::Triangle,
-                );
-
-                let target = atlas
-                    .get_mut(mip_level as usize)
-                    .expect("failed to get target by page and mip level");
-                image::imageops::replace(
-                    target,
-                    &mip_map,
-                    (size_per_block * location.x()) as i64,
-                    (size_per_block * location.y()) as i64,
-                );
-            }
-        }
+        .expect("failed to generate atlas");
 
         let bind_group_layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
             label: None,
@@ -161,14 +182,8 @@ impl UnitAtlasResource {
             ],
         });
 
-        let texture_sampler = device.create_sampler(&wgpu::SamplerDescriptor {
-            min_filter: wgpu::FilterMode::Linear,
-            mipmap_filter: wgpu::FilterMode::Linear,
-            ..Default::default()
-        });
-
         let mut bind_groups = vec![];
-        for atlas in atlas_set {
+        for atlas in atlas_result.atlas_set {
             let atlas_data = atlas
                 .iter()
                 .flat_map(|mip_map| mip_map.to_rgba8().to_vec())
@@ -179,11 +194,11 @@ impl UnitAtlasResource {
                 &wgpu::TextureDescriptor {
                     label: None,
                     size: wgpu::Extent3d {
-                        width: size,
-                        height: size,
+                        width: Self::SIZE_PER_BLOCK * Self::BLOCK_SIZE,
+                        height: Self::SIZE_PER_BLOCK * Self::BLOCK_SIZE,
                         depth_or_array_layers: 1,
                     },
-                    mip_level_count,
+                    mip_level_count: atlas_result.mip_level_count,
                     sample_count: 1,
                     dimension: wgpu::TextureDimension::D2,
                     format: wgpu::TextureFormat::Rgba8UnormSrgb,
@@ -193,6 +208,11 @@ impl UnitAtlasResource {
                 &atlas_data,
             );
             let texture_view = texture.create_view(&wgpu::TextureViewDescriptor::default());
+            let texture_sampler = device.create_sampler(&wgpu::SamplerDescriptor {
+                min_filter: wgpu::FilterMode::Linear,
+                mipmap_filter: wgpu::FilterMode::Linear,
+                ..Default::default()
+            });
 
             let bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
                 label: None,
@@ -212,8 +232,8 @@ impl UnitAtlasResource {
         }
 
         Self {
-            page_count,
-            texcoords,
+            page_count: atlas_result.page_count,
+            texcoords: atlas_result.texcoords,
             bind_group_layout,
             bind_groups,
         }
@@ -223,8 +243,8 @@ impl UnitAtlasResource {
         self.page_count
     }
 
-    pub fn texcoord(&self, item: &UnitAtlasItem) -> Option<&UnitAtlasTexcoord> {
-        self.texcoords.get(item)
+    pub fn texcoord(&self, unit_kind: &UnitKind) -> Option<&AtlasTexcoord> {
+        self.texcoords.get(unit_kind)
     }
 
     pub fn bind_group_layout(&self) -> &wgpu::BindGroupLayout {
